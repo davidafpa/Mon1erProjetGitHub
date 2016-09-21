3ca1422aaf947aecbbb170019dbffeb16897077d
DAVID <Auteur>
2016-09-21 09:22:16 +0200

    mon 1er commit

---
+++ ProjetDavidAfpaTestGit/src/TestGit.java
@@ -0,0 +1,23 @@
+/**
+ Auteur: David WOUTERS
+21 sept. 2016
+TestGit.java
+
+ProjetDavidAfpaTestGit
+ **/
+
+/**
+ * @author DAVID
+ *
+ */
+public class TestGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
